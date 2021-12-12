9c33a4ed5e21a0c229988cbf89496d2203302e47
Dustin Glaser <dustin.glaser@stud.tu-darmstadt.de>
2021-12-13 00:03:27 +0100

    add classes

---
+++ src/main/java/roommanagement/Student.java
@@ -0,0 +1,18 @@
+package roommanagement;
+
+public class Student {
+
+    public final String name;
+    public final boolean hasCertificate;
+
+    public Student(String name, boolean hasCertificate) {
+        this.name = name;
+        this.hasCertificate = hasCertificate;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name='" + name + '\'' + ", hasCertificate=" + hasCertificate + '}';
+    }
+
+}
